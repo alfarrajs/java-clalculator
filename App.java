b964ce8320324a47e7832a20d0195ae4c6b4d255
Izzedeen Alfarra <77536501+x3AZFx@users.noreply.github.com>
2023-03-06 16:47:10 +0200

    Add files via upload

---
+++ App.java
@@ -0,0 +1,31 @@
+import javax.swing.JOptionPane;
+import java.util.Random;  // to generate a random number in specific range ;
+
+public class App{
+    public static void main(String[]args){
+      Random rand = new Random();
+      while(true){
+      // random numbers in specific range  it goes from [0] to [upperbound-1]
+      int upperbound = 25;
+      int number1 = rand.nextInt(upperbound);
+      int number2 = rand.nextInt(upperbound)*2;
+      String Answer = JOptionPane.showInputDialog(null,number1+"+"+number2+"=  ?");
+      int IntAnswer = Integer.parseInt(Answer);
+      String result = (number1+number2 == IntAnswer) ? "صحيحة": "خاطئة";
+      JOptionPane.showMessageDialog(null,"  انت قلت انه     " +number1+ " + "+number2+"  بساوي  "+IntAnswer+"   الإجابة: "+result);
+      if(result == "صحيحة"){
+        break;
+      }
+      }   
+    }
+}
+
+
+
+
+
+
+
+
+
+
